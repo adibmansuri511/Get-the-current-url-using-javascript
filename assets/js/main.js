c6555b5cd1ff7d2b095d1b7a2c70bb54b0b07fe6
adibmansuri511 <adibmansuri.developer@gmail.com>
2024-01-20 22:55:35 +0530

    Update all files and folders with image.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,89 +1,24 @@
-// program to validate an email address
+// program to get the URL
 
 // Get button element and add click event listener on it and assign to element variable.
-const element = document.getElementById("myBtn");
+const myBtn = document.getElementById("myBtn");
 
-element.addEventListener("click", validateEmail);
+myBtn.addEventListener("click", getUrl);
 
 
 
-function validateEmail(e) {
+function getUrl(e) {
     e.preventDefault();
-
-    // Take input value from the user.
-    const email = document.getElementById("email").value;
 
     // Show the result on this #h5 id.
     let result = document.querySelector('#h5');
 
-    const regexPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // window.location.href: HTML DOM Window.location property returns a Location object that contains information about the current location of the document.
+    const url1 = window.location.href;
 
-    if (regexPattern.test(email)) {
-
-        console.log(`${email} is valid email address.`);
-        // result.classList.add("input-color");
-        // result.innerHTML = `${email} is valid email address.`;
-        result.innerHTML = `<span style="color: #000;">${email}</span> is valid email address.`;
+    // Document.URL: The DOM URL property in HTML is used to return a string that contains the complete URL of the current document. The string also includes the HTTP protocol such as ( http://).
+    const url2 = document.URL;
 
 
-    } else {
-
-        console.log(`${email} is not valid email address.`);
-        result.innerHTML = `<span style="color: #000;">${email}</span> is not valid email address.`;
-
-    }
-
+    result.innerHTML = `<span style="color:#031058">Current URL :</span> ${url2}`
 };
-
-
-
-// function checkNumber(e) {
-//     e.preventDefault();
-
-//     // Take input value from the user and convert it into a number.
-//     // const email = document.getElementById("email").value;
-
-//     console.log(`Input value is : ${email}`);
-
-//     // check if the passed value is a number
-//     // The typeof operator is used to check the data type of the passed value.
-//     // The isNaN() method checks if the passed value is a number.
-
-//     // Check the input field is not a blank.
-//     if (email) {
-
-
-
-//         if (typeof email === 'number' && !isNaN(email)) {
-
-//             // check if it is integer
-//             // The Number.isInteger() method is used to check if the number is an integer value.
-//             if (Number.isInteger(email)) {
-
-//                 document.querySelector('#h5').innerHTML = `Result : ${email} is integer value.`;
-
-//                 console.log(`${email} is integer value.`);
-
-//             } else {
-
-//                 document.querySelector('#h5').innerHTML = `Result : ${email} is a float value.`;
-
-//                 console.log(`${email} is a float value.`);
-
-//             }
-
-//         } else {
-
-//             document.querySelector('#h5').innerHTML = `Result : ${email} is not a number.`;
-//             console.log(`${email} is not a number.`);
-
-//         }
-
-//     } else {
-
-//         document.querySelector('#h5').innerHTML = `Please enter the value.`;
-//         console.log(`Please enter the value.`);
-
-//     }
-
-// }
